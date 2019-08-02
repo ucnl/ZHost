3aff32dd019e77223cf3567d241bcbbe8a1d1fa3
Aleksandr <aleksandr.v.dikarev@gmail.com>
2019-08-02 14:28:49 +0400

    Create AssemblyInfo.cs

---
+++ ZHost/Properties/AssemblyInfo.cs
@@ -0,0 +1,43 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ZHost")]
+[assembly: AssemblyDescription("Zima USBL underwater navigation system host application.\n\n" +
+                               "More information about Zima USBL\n" + 
+                               "English: https://github.com/ucnl/Docs/tree/master/EN/Zima\n" + 
+                               "Russian: https://github.com/ucnl/Docs/tree/master/RU/Zima\n\n" + 
+                               "Support: mailto:support@unavlab.com\n\n" +
+                               "Source code:\n" +
+                               "https://github.com/ucnl/ZHost\n" + 
+                               "https://github.com/ucnl/ZLibrary")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Underwater communication & navigation laboratory, LLC")]
+[assembly: AssemblyProduct("ZHost")]
+[assembly: AssemblyCopyright("Copyright © Underwater communication & navigation laboratory, 2019")]
+[assembly: AssemblyTrademark("UC&NL")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("8d1a4f3f-7384-4704-a9f8-ed1a585b6f6f")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("2.1.*")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
