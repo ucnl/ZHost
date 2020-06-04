266f7b118b0528322c9004c8d0c09bead2898fc8
Aleksandr <aleksandr.v.dikarev@gmail.com>
2020-06-04 12:28:33 +0400

    Add files via upload

--- ZHost/Properties/AssemblyInfo.cs
+++ ZHost/Properties/AssemblyInfo.cs
@@ -7,17 +7,28 @@ using System.Runtime.InteropServices;
 // associated with an assembly.
 [assembly: AssemblyTitle("ZHost")]
 [assembly: AssemblyDescription("Zima USBL underwater navigation system host application.\n\n" +
+                               "Latest release: https://api.github.com/repos/ucnl/ZHost/zipball \n" +
+                               "Support: mailto:support@unavlab.com\n\n" +
                                "More information about Zima USBL\n" + 
                                "English: https://github.com/ucnl/Docs/tree/master/EN/Zima\n" + 
-                               "Russian: https://github.com/ucnl/Docs/tree/master/RU/Zima\n\n" + 
-                               "Support: mailto:support@unavlab.com\n\n" +
-                               "Source code:\n" +
-                               "https://github.com/ucnl/ZHost\n" + 
-                               "https://github.com/ucnl/ZLibrary")]
+                               "Russian: https://github.com/ucnl/Docs/tree/master/RU/Zima\n\n" +                                
+                               "Project source code:\n" +
+                               "https://github.com/ucnl/ZHost\n\n" + 
+                               "This application is 100% open-source, and based on our projects:\n" +
+                               "https://github.com/ucnl/ZLibrary\n" +
+                               "https://github.com/ucnl/UCNLNav\n" +
+                               "https://github.com/ucnl/Physics\n" +
+                               "https://github.com/ucnl/UCNLNMEA\n" +
+                               "https://github.com/ucnl/KML\n" +
+                               "https://github.com/ucnl/Drivers\n" +
+                               "https://github.com/ucnl/UCNLUI"                               
+                               )]
+
+
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Underwater communication & navigation laboratory, LLC")]
 [assembly: AssemblyProduct("ZHost")]
-[assembly: AssemblyCopyright("Copyright © Underwater communication & navigation laboratory, 2019")]
+[assembly: AssemblyCopyright("Copyright © Underwater communication & navigation laboratory, 2017-2020")]
 [assembly: AssemblyTrademark("UC&NL")]
 [assembly: AssemblyCulture("")]
 
@@ -39,5 +50,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.1.*")]
+[assembly: AssemblyVersion("2.2.*")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
